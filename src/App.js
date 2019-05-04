43d8d6b2a7187ff310abb02c084a171600698223
Andrew Drumonde <andrewdrumonde@gmail.com>
2019-05-04 00:18:13 -0400

    test commit

--- src/App.js
+++ src/App.js
@@ -6,23 +6,7 @@ import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-d
 
 class App extends Component {
 
-  shuffle = (array) => {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
   
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-  
-      // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
-  
-    return array;
-  }
 
   render() {
 
